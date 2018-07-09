6b9fc5b2bf34eecf627064b3617f15991460c95e
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-07-09 21:26:23 +0430

    Implement Start, Stop, Continue, Pause and Shutdow in ServiceBase

--- WindowsServiceSample/ServiceBase.cpp
+++ WindowsServiceSample/ServiceBase.cpp
@@ -1,13 +1,31 @@
+/****************************** Module Header ******************************\
+* Module Name:  ServiceBase.cpp
+* Project:      CppWindowsService
+* Copyright (c) Microsoft Corporation.
+*
+* Provides a base class for a service that will exist as part of a service
+* application. CServiceBase must be derived from when creating a new service
+* class.
+*
+* This source is subject to the Microsoft Public License.
+* See http://www.microsoft.com/en-us/openness/resources/licenses.aspx#MPL.
+* All other rights reserved.
+*
+* THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+* EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+\***************************************************************************/
+
 #pragma region Includes
 #include "stdafx.h"
 #include <assert.h>
+#include <strsafe.h>
 #include <Windows.h>
 #include "ServiceBase.h"
 #pragma endregion
 
 
-// Initialize the singleton service instance.
-CServiceBase *CServiceBase::service = NULL;
+#pragma region Service Constructor and Destructor
 
 //
 //   FUNCTION: CServiceBase::CServiceBase(PWSTR, BOOL, BOOL, BOOL)
@@ -56,9 +74,22 @@ CServiceBase::CServiceBase(PWSTR pszServiceName,
 	m_Status.dwWaitHint = 0;
 }
 
-CServiceBase::~CServiceBase()
+//
+//   FUNCTION: CServiceBase::~CServiceBase()
+//
+//   PURPOSE: The virtual destructor of CServiceBase. 
+//
+CServiceBase::~CServiceBase(void)
 {
 }
+
+#pragma endregion
+
+
+#pragma region Static Members
+
+// Initialize the singleton service instance.
+CServiceBase *CServiceBase::service = NULL;
 
 //
 //   FUNCTION: CServiceBase::Run(CServiceBase &)
@@ -93,6 +124,16 @@ BOOL CServiceBase::Start(CServiceBase &service)
 	return StartServiceCtrlDispatcher(serviceTable);
 }
 
+//
+//   FUNCTION: CServiceBase::ServiceMain(DWORD, PWSTR *)
+//
+//   PURPOSE: Entry point for the service. It registers the handler function 
+//   for the service and starts the service.
+//
+//   PARAMETERS:
+//   * dwArgc   - number of command line arguments
+//   * lpszArgv - array of command line arguments
+//
 void CServiceBase::ServiceMain(DWORD dwArgc, LPWSTR *pszArgv)
 {
 	assert(service != NULL);
@@ -108,6 +149,29 @@ void CServiceBase::ServiceMain(DWORD dwArgc, LPWSTR *pszArgv)
 	service->Start(dwArgc, pszArgv);
 }
 
+//
+//   FUNCTION: CServiceBase::ServiceCtrlHandler(DWORD)
+//
+//   PURPOSE: The function is called by the SCM whenever a control code is 
+//   sent to the service. 
+//
+//   PARAMETERS:
+//   * dwCtrlCode - the control code. This parameter can be one of the 
+//   following values: 
+//
+//     SERVICE_CONTROL_CONTINUE
+//     SERVICE_CONTROL_INTERROGATE
+//     SERVICE_CONTROL_NETBINDADD
+//     SERVICE_CONTROL_NETBINDDISABLE
+//     SERVICE_CONTROL_NETBINDREMOVE
+//     SERVICE_CONTROL_PARAMCHANGE
+//     SERVICE_CONTROL_PAUSE
+//     SERVICE_CONTROL_SHUTDOWN
+//     SERVICE_CONTROL_STOP
+//
+//   This parameter can also be a user-defined control code ranges from 128 
+//   to 255.
+//
 void CServiceBase::ServiceCtrlHandler(DWORD dwCtrl)
 {
 	switch (dwCtrl)
@@ -131,7 +195,70 @@ void CServiceBase::ServiceCtrlHandler(DWORD dwCtrl)
 	}
 }
 
+#pragma endregion
+
+
+#pragma region Service Start, Stop, Pause, Continue, and Shutdown
+
+//
+//   FUNCTION: CServiceBase::Start(DWORD, PWSTR *)
+//
+//   PURPOSE: The function starts the service. It calls the OnStart virtual 
+//   function in which you can specify the actions to take when the service 
+//   starts. If an error occurs during the startup, the error will be logged 
+//   in the Application event log, and the service will be stopped.
+//
+//   PARAMETERS:
+//   * dwArgc   - number of command line arguments
+//   * lpszArgv - array of command line arguments
+//
 void CServiceBase::Start(DWORD dwArgc, PWSTR *pszArgv)
+{
+	try
+	{
+		// Tell SCM that the service is starting.
+		SetServiceStatus(SERVICE_START_PENDING);
+
+		// Perform service-specific initialization.
+		OnStart(dwArgc, pszArgv);
+
+		// Tell SCM that the service is started.
+		SetServiceStatus(SERVICE_RUNNING);
+	}
+	catch (DWORD dwError)
+	{
+		// Log the error.
+		WriteErrorLogEntry(L"Service Start", dwError);
+
+		// Set the service status to be stopped.
+		SetServiceStatus(SERVICE_STOPPED, dwError);
+	}
+	catch (...)
+	{
+		// Log the error.
+		WriteEventLogEntry(L"Service failed to start.", EVENTLOG_ERROR_TYPE);
+
+		// Set the service status to be stopped.
+		SetServiceStatus(SERVICE_STOPPED);
+	}
+}
+
+//
+//   FUNCTION: CServiceBase::OnStart(DWORD, PWSTR *)
+//
+//   PURPOSE: When implemented in a derived class, executes when a Start 
+//   command is sent to the service by the SCM or when the operating system 
+//   starts (for a service that starts automatically). Specifies actions to 
+//   take when the service starts. Be sure to periodically call 
+//   CServiceBase::SetServiceStatus() with SERVICE_START_PENDING if the 
+//   procedure is going to take long time. You may also consider spawning a 
+//   new thread in OnStart to perform time-consuming initialization tasks.
+//
+//   PARAMETERS:
+//   * dwArgc   - number of command line arguments
+//   * lpszArgv - array of command line arguments
+//
+void CServiceBase::OnStart(DWORD dwArgc, PWSTR * pszArgv)
 {
 }
 
@@ -145,16 +272,289 @@ void CServiceBase::Start(DWORD dwArgc, PWSTR *pszArgv)
 //
 void CServiceBase::Stop() 
 {
+	DWORD dwOriginalState = m_Status.dwCurrentState;
+	try
+	{
+		// Tell SCM that the service is stopping.
+		SetServiceStatus(SERVICE_STOP_PENDING);
+
+		// Perform service-specific stop operations.
+		OnStop();
+
+		// Tell SCM that the service is stopped.
+		SetServiceStatus(SERVICE_STOPPED);
+	}
+	catch (DWORD dwError)
+	{
+		// Log the error.
+		WriteErrorLogEntry(L"Service Stop", dwError);
+
+		// Set the orginal service status.
+		SetServiceStatus(dwOriginalState);
+	}
+	catch (...)
+	{
+		// Log the error.
+		WriteEventLogEntry(L"Service failed to stop.", EVENTLOG_ERROR_TYPE);
+
+		// Set the orginal service status.
+		SetServiceStatus(dwOriginalState);
+	}
 }
 
+//
+//   FUNCTION: CServiceBase::OnStop()
+//
+//   PURPOSE: When implemented in a derived class, executes when a Stop 
+//   command is sent to the service by the SCM. Specifies actions to take 
+//   when a service stops running. Be sure to periodically call 
+//   CServiceBase::SetServiceStatus() with SERVICE_STOP_PENDING if the 
+//   procedure is going to take long time. 
+//
+void CServiceBase::OnStop()
+{
+}
+
+//
+//   FUNCTION: CServiceBase::Pause()
+//
+//   PURPOSE: The function pauses the service if the service supports pause 
+//   and continue. It calls the OnPause virtual function in which you can 
+//   specify the actions to take when the service pauses. If an error occurs, 
+//   the error will be logged in the Application event log, and the service 
+//   will become running.
+//
 void CServiceBase::Pause()
 {
+	try
+	{
+		// Tell SCM that the service is pausing.
+		SetServiceStatus(SERVICE_PAUSE_PENDING);
+
+		// Perform service-specific pause operations.
+		OnPause();
+
+		// Tell SCM that the service is paused.
+		SetServiceStatus(SERVICE_PAUSED);
+	}
+	catch (DWORD dwError)
+	{
+		// Log the error.
+		WriteErrorLogEntry(L"Service Pause", dwError);
+
+		// Tell SCM that the service is still running.
+		SetServiceStatus(SERVICE_RUNNING);
+	}
+	catch (...)
+	{
+		// Log the error.
+		WriteEventLogEntry(L"Service failed to pause.", EVENTLOG_ERROR_TYPE);
+
+		// Tell SCM that the service is still running.
+		SetServiceStatus(SERVICE_RUNNING);
+	}
 }
 
+//
+//   FUNCTION: CServiceBase::OnPause()
+//
+//   PURPOSE: When implemented in a derived class, executes when a Pause 
+//   command is sent to the service by the SCM. Specifies actions to take 
+//   when a service pauses.
+//
+void CServiceBase::OnPause()
+{
+}
+
+//
+//   FUNCTION: CServiceBase::Continue()
+//
+//   PURPOSE: The function resumes normal functioning after being paused if
+//   the service supports pause and continue. It calls the OnContinue virtual 
+//   function in which you can specify the actions to take when the service 
+//   continues. If an error occurs, the error will be logged in the 
+//   Application event log, and the service will still be paused.
+//
 void CServiceBase::Continue()
 {
+	try
+	{
+		// Tell SCM that the service is resuming.
+		SetServiceStatus(SERVICE_CONTINUE_PENDING);
+
+		// Perform service-specific continue operations.
+		OnContinue();
+
+		// Tell SCM that the service is running.
+		SetServiceStatus(SERVICE_RUNNING);
+	}
+	catch (DWORD dwError)
+	{
+		// Log the error.
+		WriteErrorLogEntry(L"Service Continue", dwError);
+
+		// Tell SCM that the service is still paused.
+		SetServiceStatus(SERVICE_PAUSED);
+	}
+	catch (...)
+	{
+		// Log the error.
+		WriteEventLogEntry(L"Service failed to resume.", EVENTLOG_ERROR_TYPE);
+
+		// Tell SCM that the service is still paused.
+		SetServiceStatus(SERVICE_PAUSED);
+	}
 }
 
-void CServiceBase::Shutdown()
+//
+//   FUNCTION: CServiceBase::OnContinue()
+//
+//   PURPOSE: When implemented in a derived class, OnContinue runs when a 
+//   Continue command is sent to the service by the SCM. Specifies actions to 
+//   take when a service resumes normal functioning after being paused.
+//
+void CServiceBase::OnContinue()
 {
 }
+
+//
+//   FUNCTION: CServiceBase::Shutdown()
+//
+//   PURPOSE: The function executes when the system is shutting down. It 
+//   calls the OnShutdown virtual function in which you can specify what 
+//   should occur immediately prior to the system shutting down. If an error 
+//   occurs, the error will be logged in the Application event log.
+//
+void CServiceBase::Shutdown()
+{
+	try
+	{
+		// Perform service-specific shutdown operations.
+		OnShutdown();
+
+		// Tell SCM that the service is stopped.
+		SetServiceStatus(SERVICE_STOPPED);
+	}
+	catch (DWORD dwError)
+	{
+		// Log the error.
+		WriteErrorLogEntry(L"Service Shutdown", dwError);
+	}
+	catch (...)
+	{
+		// Log the error.
+		WriteEventLogEntry(L"Service failed to shut down.", EVENTLOG_ERROR_TYPE);
+	}
+}
+
+//
+//   FUNCTION: CServiceBase::OnShutdown()
+//
+//   PURPOSE: When implemented in a derived class, executes when the system 
+//   is shutting down. Specifies what should occur immediately prior to the 
+//   system shutting down.
+//
+void CServiceBase::OnShutdown()
+{
+}
+
+#pragma endregion
+
+
+#pragma region Helper Functions
+
+//
+//   FUNCTION: CServiceBase::SetServiceStatus(DWORD, DWORD, DWORD)
+//
+//   PURPOSE: The function sets the service status and reports the status to 
+//   the SCM.
+//
+//   PARAMETERS:
+//   * dwCurrentState - the state of the service
+//   * dwWin32ExitCode - error code to report
+//   * dwWaitHint - estimated time for pending operation, in milliseconds
+//
+void CServiceBase::SetServiceStatus(DWORD dwCurrentState,
+	DWORD dwWin32ExitCode,
+	DWORD dwWaitHint)
+{
+	static DWORD dwCheckPoint = 1;
+
+	// Fill in the SERVICE_STATUS structure of the service.
+
+	m_Status.dwCurrentState = dwCurrentState;
+	m_Status.dwWin32ExitCode = dwWin32ExitCode;
+	m_Status.dwWaitHint = dwWaitHint;
+
+	m_Status.dwCheckPoint =
+		((dwCurrentState == SERVICE_RUNNING) ||
+		(dwCurrentState == SERVICE_STOPPED)) ?
+		0 : dwCheckPoint++;
+
+	// Report the status of the service to the SCM.
+	::SetServiceStatus(m_StatusHandle, &m_Status);
+}
+
+
+//
+//   FUNCTION: CServiceBase::WriteEventLogEntry(PWSTR, WORD)
+//
+//   PURPOSE: Log a message to the Application event log.
+//
+//   PARAMETERS:
+//   * pszMessage - string message to be logged.
+//   * wType - the type of event to be logged. The parameter can be one of 
+//     the following values.
+//
+//     EVENTLOG_SUCCESS
+//     EVENTLOG_AUDIT_FAILURE
+//     EVENTLOG_AUDIT_SUCCESS
+//     EVENTLOG_ERROR_TYPE
+//     EVENTLOG_INFORMATION_TYPE
+//     EVENTLOG_WARNING_TYPE
+//
+void CServiceBase::WriteEventLogEntry(PWSTR pszMessage, WORD wType)
+{
+	HANDLE hEventSource = NULL;
+	LPCWSTR lpszStrings[2] = { NULL, NULL };
+
+	hEventSource = RegisterEventSource(NULL, m_Name);
+	if (hEventSource)
+	{
+		lpszStrings[0] = m_Name;
+		lpszStrings[1] = pszMessage;
+
+		ReportEvent(hEventSource,  // Event log handle
+			wType,                 // Event type
+			0,                     // Event category
+			0,                     // Event identifier
+			NULL,                  // No security identifier
+			2,                     // Size of lpszStrings array
+			0,                     // No binary data
+			lpszStrings,           // Array of strings
+			NULL                   // No binary data
+		);
+
+		DeregisterEventSource(hEventSource);
+	}
+}
+
+
+//
+//   FUNCTION: CServiceBase::WriteErrorLogEntry(PWSTR, DWORD)
+//
+//   PURPOSE: Log an error message to the Application event log.
+//
+//   PARAMETERS:
+//   * pszFunction - the function that gives the error
+//   * dwError - the error code
+//
+void CServiceBase::WriteErrorLogEntry(PWSTR pszFunction, DWORD dwError)
+{
+	wchar_t szMessage[260];
+	StringCchPrintf(szMessage, ARRAYSIZE(szMessage),
+		L"%s failed w/err 0x%08lx", pszFunction, dwError);
+	WriteEventLogEntry(szMessage, EVENTLOG_ERROR_TYPE);
+}
+
+#pragma endregion
--- WindowsServiceSample/ServiceBase.h
+++ WindowsServiceSample/ServiceBase.h
@@ -40,6 +40,48 @@ public:
 	// method blocks until the service has stopped.
 	static BOOL Start(CServiceBase &service);
 
+protected:
+
+	// When implemented in a derived class, executes when a Start command is 
+	// sent to the service by the SCM or when the operating system starts 
+	// (for a service that starts automatically). Specifies actions to take 
+	// when the service starts.
+	virtual void OnStart(DWORD dwArgc, PWSTR *pszArgv);
+
+	// When implemented in a derived class, executes when a Stop command is 
+	// sent to the service by the SCM. Specifies actions to take when a 
+	// service stops running.
+	virtual void OnStop();
+
+	// When implemented in a derived class, executes when a Pause command is 
+	// sent to the service by the SCM. Specifies actions to take when a 
+	// service pauses.
+	virtual void OnPause();
+
+	// When implemented in a derived class, OnContinue runs when a Continue 
+	// command is sent to the service by the SCM. Specifies actions to take 
+	// when a service resumes normal functioning after being paused.
+	virtual void OnContinue();
+
+	// When implemented in a derived class, executes when the system is 
+	// shutting down. Specifies what should occur immediately prior to the 
+	// system shutting down.
+	virtual void OnShutdown();
+
+protected:
+
+	// Set the service status and report the status to the SCM.
+	void SetServiceStatus(DWORD dwCurrentState,
+		DWORD dwWin32ExitCode = NO_ERROR,
+		DWORD dwWaitHint = 0);
+
+	// Log a message to the Application event log.
+	void WriteEventLogEntry(PWSTR pszMessage, WORD wType);
+
+	// Log an error message to the Application event log.
+	void WriteErrorLogEntry(PWSTR pszFunction,
+		DWORD dwError = GetLastError());
+
 private:
 
 	// The singleton service instance.

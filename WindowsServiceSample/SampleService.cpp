d9b5b20d5e4766a595801ceec9c57ef4dc4442a7
Hossein Mohammadi <hussein.muhammadi@hotmail.com>
2018-07-13 11:25:51 +0430

    Using thread pool to run service main function in a new thread

--- WindowsServiceSample/SampleService.cpp
+++ WindowsServiceSample/SampleService.cpp
@@ -1,8 +1,8 @@
-
 #pragma region Includes
 #include "stdafx.h"
 #include <Windows.h>
 #include "ServiceBase.h"
+#include "ThreadPool.h"
 #include "SampleService.h"
 #pragma endregion
 
@@ -23,4 +23,94 @@ CSampleService::CSampleService(PWSTR pszServiceName, BOOL fCanStop, BOOL fCanShu
 
 CSampleService::~CSampleService()
 {
+	if (m_hStoppedEvent)
+	{
+		CloseHandle(m_hStoppedEvent);
+		m_hStoppedEvent = NULL;
+	}
+}
+
+
+//
+//   FUNCTION: CSampleService::OnStart(DWORD, LPWSTR *)
+//
+//   PURPOSE: The function is executed when a Start command is sent to the 
+//   service by the SCM or when the operating system starts (for a service 
+//   that starts automatically). It specifies actions to take when the 
+//   service starts. In this code sample, OnStart logs a service-start 
+//   message to the Application log, and queues the main service function for 
+//   execution in a thread pool worker thread.
+//
+//   PARAMETERS:
+//   * dwArgc   - number of command line arguments
+//   * lpszArgv - array of command line arguments
+//
+//   NOTE: A service application is designed to be long running. Therefore, 
+//   it usually polls or monitors something in the system. The monitoring is 
+//   set up in the OnStart method. However, OnStart does not actually do the 
+//   monitoring. The OnStart method must return to the operating system after 
+//   the service's operation has begun. It must not loop forever or block. To 
+//   set up a simple monitoring mechanism, one general solution is to create 
+//   a timer in OnStart. The timer would then raise events in your code 
+//   periodically, at which time your service could do its monitoring. The 
+//   other solution is to spawn a new thread to perform the main service 
+//   functions, which is demonstrated in this code sample.
+//
+void CSampleService::OnStart(DWORD dwArgc, PWSTR *pszArgv)
+{
+	// Log a service start message to the Application log.
+	WriteEventLogEntry(L"CppWindowsService in OnStart",
+		EVENTLOG_INFORMATION_TYPE);
+
+	// Queue the main service function for execution in a worker thread.
+	CThreadPool::QueueUserWorkItem(&CSampleService::ServiceWorkerThread, this);
+}
+
+
+//
+//   FUNCTION: CSampleService::ServiceWorkerThread(void)
+//
+//   PURPOSE: The method performs the main function of the service. It runs 
+//   on a thread pool worker thread.
+//
+void CSampleService::ServiceWorkerThread(void)
+{
+	// Periodically check if the service is stopping.
+	while (!m_fStopping)
+	{
+		// Perform main service function here...
+
+		::Sleep(2000);  // Simulate some lengthy operations.
+	}
+
+	// Signal the stopped event.
+	SetEvent(m_hStoppedEvent);
+}
+
+
+//
+//   FUNCTION: CSampleService::OnStop(void)
+//
+//   PURPOSE: The function is executed when a Stop command is sent to the 
+//   service by SCM. It specifies actions to take when a service stops 
+//   running. In this code sample, OnStop logs a service-stop message to the 
+//   Application log, and waits for the finish of the main service function.
+//
+//   COMMENTS:
+//   Be sure to periodically call ReportServiceStatus() with 
+//   SERVICE_STOP_PENDING if the procedure is going to take long time. 
+//
+void CSampleService::OnStop()
+{
+	// Log a service stop message to the Application log.
+	WriteEventLogEntry(L"CppWindowsService in OnStop",
+		EVENTLOG_INFORMATION_TYPE);
+
+	// Indicate that the service is stopping and wait for the finish of the 
+	// main service function (ServiceWorkerThread).
+	m_fStopping = TRUE;
+	if (WaitForSingleObject(m_hStoppedEvent, INFINITE) != WAIT_OBJECT_0)
+	{
+		throw GetLastError();
+	}
 }
--- WindowsServiceSample/SampleService.h
+++ WindowsServiceSample/SampleService.h
@@ -30,6 +30,13 @@ public:
 
 	virtual ~CSampleService();
 
+protected:
+
+	virtual void OnStart(DWORD dwArgc, PWSTR *pszArgv);
+	virtual void OnStop();
+
+	void ServiceWorkerThread(void);
+
 private:
 	BOOL m_fStopping;
 	HANDLE m_hStoppedEvent;
